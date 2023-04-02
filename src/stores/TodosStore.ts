80d7235888069dad76d219ea91821dad92b7cac5
Hardik <sachanhardik@gmail.com>
2023-04-02 18:11:55 +0530

    added todos store

---
+++ src/stores/TodosStore.ts
@@ -0,0 +1,28 @@
+import { createStore, produce } from "solid-js/store";
+import { v4 as uuid } from "uuid";
+
+interface Todo {
+  id: string;
+  task: string;
+  complete: boolean;
+}
+
+const [todos, setTodos] = createStore<Todo[]>([]);
+
+function addTodo(task: string) {
+  const todo = { id: uuid(), task, complete: false };
+  setTodos(produce((todos) => todos.push(todo)));
+}
+
+function toggleTodo(id: string) {
+  setTodos(
+    (todo) => todo.id === id,
+    produce((todo) => (todo.complete = !todo.complete))
+  );
+}
+
+function removeTodo(id: string) {
+  setTodos(produce((todos) => todos.filter((todo) => todo.id !== id)));
+}
+
+export { todos, addTodo, toggleTodo, removeTodo };
